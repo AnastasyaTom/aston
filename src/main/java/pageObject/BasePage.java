5c699f1bf2cde96f791b8b1e0d5384d97af0f782
Anastasia Tomashevich <nastena.kolesnikova.92@mail.ru>
2025-08-11 09:44:30 +0300

    Lesson 10 (MTS)

---
+++ src/main/java/pageObject/BasePage.java
@@ -0,0 +1,20 @@
+package pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    BasePage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    protected WebElement getElement(String locator) {
+        return driver.findElement(By.xpath(locator));
+    }
+}
